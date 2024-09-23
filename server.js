4ddfb40386bc42287f3348c62bf3a2de3f4c2879
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2024-09-23 21:50:25 +0530

    changes api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors=require('cors');
 
 const cronJobs = require('./mailConfig/cronjob');
 const app = express()
-const port = 3000
+const port = 10000
 
 
 app.use(cors());
@@ -11,11 +11,13 @@ app.use(express.json());
 
 const route=require('./router/router')
 const mailRouter=require('./router/mailRouter')
+const hospiatlRouter=require('./router/hospitalRouter')
 
 
 
 
 app.use('/base',route)
 app.use('/mail',mailRouter)
+app.use('/hospital',hospiatlRouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
